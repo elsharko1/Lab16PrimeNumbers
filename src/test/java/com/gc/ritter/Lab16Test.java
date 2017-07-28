d27b92cb65c7493f567568a2e62ea55f9363c296
elsharko1 <elsharko1@gmail.com>
2017-07-28 09:18:21 -0400

    added some tests

--- src/test/java/com/gc/ritter/Lab16Test.java
+++ src/test/java/com/gc/ritter/Lab16Test.java
@@ -18,6 +18,26 @@ public class Lab16Test {
 
         assertEquals("It's not prime", expected, actual);
     }
+    @Test
+    public void isPrime2() throws Exception {
+
+        Lab16 lab16 = new Lab16();
+        boolean expected = true;
+        boolean actual = Lab16.isPrime(13);
+
+
+        assertEquals("It's not prime", expected, actual);
+    }
+    @Test
+    public void isPrime3() throws Exception {
+
+        Lab16 lab16 = new Lab16();
+        boolean expected = true;
+        boolean actual = Lab16.isPrime(17);
+
+
+        assertEquals("It's not prime", expected, actual);
+    }
 
     @Test
     public void sequencePrime() throws Exception {
@@ -28,5 +48,24 @@ public class Lab16Test {
 
         assertEquals("Not working", expected, actual);
     }
+    @Test
+    public void sequencePrime2() throws Exception {
+        Lab16 lab16 = new Lab16();
+        int expected = 5;
+        int actual = Lab16.sequencePrime(3);
+
+
+        assertEquals("Not working", expected, actual);
+    }
+    @Test
+    public void sequencePrime3() throws Exception {
+        Lab16 lab16 = new Lab16();
+        int expected = 11;
+        int actual = Lab16.sequencePrime(5);
+
+
+        assertEquals("Not working", expected, actual);
+    }
+
 
 }
